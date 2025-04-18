bd21152a9fe4540e87b8cf420e1c5f8a72f523bb
Fragon02 <chj20202@naver.com>
2025-04-18 20:24:02 +0900

    ãeat:  ìttern 추가

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -13,8 +13,9 @@ public class UserChangePasswordRequest {
     @NotBlank
     private String oldPassword;
 
+    //lv1-2
     @NotBlank
-    @Pattern(reqxp="^(?=.*[A-Z])(?=.*[a-z])(?=.*[\\d])[A-Za-z\\d]{8,20}$",
+    @Pattern(reqxp="^(?=.*[A-Z])(?=.*[a-z])(?=.*[\\d])[A-Za-z\\d]{8,}$",
             message = "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다." )
     private String newPassword;
 }
