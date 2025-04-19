6d436e781983c79bb643d9542fb22c443770ca3d
Fragon02 <chj20202@naver.com>
2025-04-20 05:44:25 +0900

    fix: lv2 problem

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +16,7 @@ public class UserChangePasswordRequest {
 
     //lv1-2
     @NotBlank
-    @Pattern(reqxp="^(?=.*[A-Z])(?=.*[a-z])(?=.*[\\d])[A-Za-z\\d]{8,}$",
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[\\d])[A-Za-z\\d]{8,}$",
             message = "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다." )
     private String newPassword;
 }
