a7d1676ce0d7202e8ac88b15a4b02ba7c56aa880
Fragon02 <chj20202@naver.com>
2025-04-20 19:55:21 +0900

    lv1 -3 코드 개선 퀴즈 - Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserChangePasswordRequest {
 
+    //lv1 -3 코드 개선 퀴즈 - Validation
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[\\d])[A-Za-z\\d]{8,}$",
+            message = "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다." )
     @NotBlank
     private String oldPassword;
+
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[\\d])[A-Za-z\\d]{8,}$",
+            message = "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다." )
     @NotBlank
     private String newPassword;
 }
